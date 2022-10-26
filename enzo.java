22cd2934ca4899999b8c2c89bdba948749f2381b
astridhidalgo <astrid.ahm29@gmail.com>
2022-10-26 13:17:00 -0300

    Commit edicion archivo enzo

--- enzo.java
+++ enzo.java
@@ -2,5 +2,10 @@ public class enzo{
     String name enzo = "enzo";
     String name quienLoHizo = "enzo";
         System.out.println(enzo +":::"+quienLoHizo);
+ System.out.println("Astrid" +":::"+quienLoHizo);
 
 }
+
+
+
+
